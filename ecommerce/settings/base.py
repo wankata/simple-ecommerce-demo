b54163e95c1aea89e7b926714dd8b801d6159ffa
Ivan Ivanov <wankata@ivanovi.net>
2019-01-18 22:26:38 +0200

    Added Catalogue app to INSTALLED_APPS

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # our own apps
+    'catalogue.apps.CatalogueConfig',
 ]
 
 MIDDLEWARE = [
