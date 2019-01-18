cee4505946a107af88f3ec3ed4b1a8ddb4dca3d8
Ivan Ivanov <wankata@ivanovi.net>
2019-01-19 01:16:44 +0200

    Register Category as MPTTModelAdmin in the admin panel. Now we have:
    - Nodes indented in tree view
    - We cannot change the parent category to one of the current category's children.

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from mptt.admin import MPTTModelAdmin
 from .models import Category
 
-admin.site.register(Category)
+admin.site.register(Category, MPTTModelAdmin)
