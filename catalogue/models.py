fd936f04da6ecf96b39947280a58c8f8ea584b1a
Ivan Ivanov <wankata@ivanovi.net>
2019-01-18 23:36:30 +0200

    Allow categories without parent, ergo top level categories.

--- catalogue/models.py
+++ catalogue/models.py
@@ -3,4 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     title = models.CharField(max_length=100)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE)
+    parent = models.ForeignKey('self',
+                               on_delete=models.CASCADE,
+                               blank=True,
+                               null=True)
