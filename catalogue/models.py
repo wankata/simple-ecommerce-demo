9e9c13ac3e2f76378780dce23c5f80e857bd144f
Ivan Ivanov <wankata@ivanovi.net>
2019-01-19 01:01:01 +0200

    Converted Category model to MPTTModel. Added migration.
    Consider giving reasonable one time defaults for level (0), left (1), right (2), three_id (0):
    If you have some data already you may need to rebuild the tree (TreeManager.rebuild()).

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
 
-class Category(models.Model):
+class Category(MPTTModel):
     title = models.CharField(max_length=100)
-    parent = models.ForeignKey('self',
-                               on_delete=models.CASCADE,
-                               blank=True,
-                               null=True,
-                               verbose_name="Parent category")
+    parent = TreeForeignKey('self',
+                            on_delete=models.CASCADE,
+                            blank=True,
+                            null=True,
+                            verbose_name="Parent category")
 
     class Meta:
         unique_together = ("title", "parent")
