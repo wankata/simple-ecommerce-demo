8161e420c1c83a271ed88acc3c734dfc7f93bd91
Ivan Ivanov <wankata@ivanovi.net>
2019-01-19 01:25:38 +0200

    Changed Category model to DraggableMPTTAdmin in admin panel. Now we:
    - May reorder the categories with drag and drop
    - May expand and collapse the category levels by need

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from mptt.admin import MPTTModelAdmin
+from mptt.admin import DraggableMPTTAdmin
 from .models import Category
 
-admin.site.register(Category, MPTTModelAdmin)
+admin.site.register(Category,
+                    DraggableMPTTAdmin,
+                    list_display=(
+                        'tree_actions',
+                        'indented_title'
+                    ),
+                    list_display_links=(
+                        'indented_title',))
