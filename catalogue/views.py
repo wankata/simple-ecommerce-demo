4d14b73daef2853107dfb105b23c08321707e144
Ivan Ivanov <wankata@ivanovi.net>
2019-01-21 02:16:34 +0200

    The active category in the menu is now visible from the product detail page:
    - Added category in the template's context, because otherwise we need to
      override the whole menu just to change the "category" with "product.category"
    - The current category is now an anchor so we can navigate back to it from the
      product's detail page

--- catalogue/views.py
+++ catalogue/views.py
@@ -15,3 +15,8 @@ class CategoryDetail(DetailView):
 class ProductDetail(DetailView):
     model = Product
     context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["category"] = self.get_object().category
+        return context
