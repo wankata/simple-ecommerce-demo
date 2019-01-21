219842ba0a47b6aca56c361fd5d6a887f0c6a3b3
Ivan Ivanov <wankata@ivanovi.net>
2019-01-21 03:06:02 +0200

    Added Index page inside project.

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -18,7 +18,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import Index
+
 urlpatterns = [
+    path('', Index.as_view(), name='index'),
     path('catalogue/', include('catalogue.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
